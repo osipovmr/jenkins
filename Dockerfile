e0de7bd045d88a5b80ac1c8a12d9b8c20acc65c2
Осипов Илья Александрович <osipovmr@yandex.ru>
2024-07-03 23:45:39 +0300

    stage('Run Docker Container')

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM openjdk:17-jdk-alpine
 WORKDIR /app
 COPY target/jenkins-1.0-SNAPSHOT.jar /app/jenkins.jar
+EXPOSE 8888
 ENTRYPOINT ["java", "-jar", "jenkins.jar"]
 
 
---
+++ src/main/java/osipovmr/controller/Controller.java
@@ -0,0 +1,15 @@
+package osipovmr.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+}
